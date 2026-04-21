00259085958c3276f78f89c516af1b23d83b2605
tkddbs1211 <a01040971164@gmail.com>
2026-04-21 09:53:40 +0900

    Update sparseMatrix.c

--- sparseMatrix.c
+++ sparseMatrix.c
@@ -29,7 +29,7 @@ sparseMatrix* addSparseMatrix(sparseMatrix* sm1, sparseMatrix* sm2) {
 			= getItemArrayList(sm1, i);
 
 		int j;
-		for (j = 0; j < sizeArrayList(sm2); j++) {//오류수정 j<=sizeArrayList(sm2) <=을 <fh 수정했습니다.
+		for (j = 0; j < sizeArrayList(sm2); j++) {//오류수정 j<=sizeArrayList(sm2) <=을 <로 수정했습니다.
 			elementArrayList nonZeroOfSM2
 				= getItemArrayList(sm2, j);
 
@@ -50,6 +50,23 @@ sparseMatrix* addSparseMatrix(sparseMatrix* sm1, sparseMatrix* sm2) {
 				nonZeroOfSM1);
 		}
 	}
+
+	for (int k = 0; k < sizeArrayList(sm2); k++) {
+	elementArrayList item2 = getItemArrayList(sm2, k); //오류 수정 sm2의 값이 누락되는 경우있기에 오류 수정
+	int found = 0;
+
+	for (int i = 0; i < sizeArrayList(sm1); i++) {
+		elementArrayList item1 = getItemArrayList(sm1, i);
+		if (item1.row == item2.row && item1.col == item2.col) {
+			found = 1;
+			break;
+		}
+	}
+
+	if (!found) { 
+		insertArrayList(smResult, sizeArrayList(smResult), item2);
+	}
+}
 
 	return smResult;
 }
